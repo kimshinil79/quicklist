93b138ccd73621dcf5c2c46560e3b04b21090351
ShinIl <alienpro@naver.com>
2022-03-13 09:24:49 +0900

    homepate ts basic frame

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { AlertController, IonContent, IonList, NavController } from '@ionic/angular';
+import { ChecklistService } from '../services/checklist.service';
+import { Checklist } from '../interfaces/checklist';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild(IonList, {static: false}) slidingList: IonList;
+  @ViewChild(IonContent, {static: false}) contentArea: IonContent;
 
-  constructor() {}
+  public checklists:Checklist[] = [];
+
+  constructor(
+    private checklistService: ChecklistService,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController
+  ) {}
+
+  ngOnInit() {
+    this.checklistService.getChecklists().subscribe((checklists) => {
+      this.checklists = checklists;
+    });
+  }
+
+  async addChecklist(): Promise<void> {}
+
+  async renameChecklist(checklist: Checklist): Promise<void> {}
+
+  async removeChecklist(checklist: Checklist): Promise<void> {}
 
 }
