af2b1a20ef76209de31dee9c7090ce03abec3d8b
ShinIl <alienpro@naver.com>
2022-03-14 05:46:46 +0900

    homepate ts file and html complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -26,10 +26,63 @@ export class HomePage implements OnInit {
     });
   }
 
-  async addChecklist(): Promise<void> {}
+  async addChecklist(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header: "New Checklist",
+      message: "Enter the name of your new checklist below:",
+      inputs: [
+        {
+          type: "text",
+          name: "name",
+        },
+      ],
+      buttons: [
+        {
+          text:"Cancel"
+        },
+        {
+          text: "Save",
+          handler:async (data) => {
+            await this.checklistService.createChecklist(data.name);
+            this.contentArea.scrollToBottom(300);
+          },
+        },
+      ],
+    });
 
-  async renameChecklist(checklist: Checklist): Promise<void> {}
+    alert.present();
+  }
 
-  async removeChecklist(checklist: Checklist): Promise<void> {}
+  async renameChecklist(checklist: Checklist): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header: "Rename Checklist",
+      message: "Enter the new name of this checklist below:",
+      inputs:[
+        {
+          type: "text",
+          name: "name",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Save",
+          handler: async (data) => {
+            await this.checklistService.updateChecklist(checklist.id, data.name);
+            this.slidingList.closeSlidingItems();
+          },
+        },
+      ],
+    });
+
+    alert.present();
+  }
+
+  async removeChecklist(checklist: Checklist): Promise<void> {
+    await this.slidingList.closeSlidingItems();
+    this.checklistService.removeChecklist(checklist.id);
+  }
 
 }
